dfef8e8b0211d5fc3ac72b097a65fee9d5b8478e
aarmandhal <adhal@ualberta.ca>
2026-04-02 00:18:54 -0600

    Updated main.py to use run_synthesizer_agent

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import argparse
 from agents.researcher import run_research_agent
+from agents.synthesizer import run_synthesizer_agent
 
 def main():
-    results = []
     parser = argparse.ArgumentParser(description="Autonomous Competitive Research Swarm")
     parser.add_argument("--topic", type=str, required=True, help="Topic you want to research. Separate multiple topics with commas.")
     parser.add_argument("--persona", type=str, required=True, help="The persona of the agent")
     args = parser.parse_args()
 
     topics = [t.strip() for t in args.topic.split(",")]
-    for topic in topics:
-        result = run_research_agent(topic, args.persona)
-        results.append(result)
-    
-    print(results)
+    final_result = run_synthesizer_agent(topics, args.persona)
+    print(final_result)
 
 if __name__ == "__main__":
     main()
