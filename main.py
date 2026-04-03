279d49dbe7de649eabac66ce754648928a462089
aarmandhal <adhal@ualberta.ca>
2026-04-03 14:15:59 -0600

    Use Console and Markdown function from rich library in main.py implementation

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import argparse
+from rich import Console, Markdown
 from agents.synthesizer import run_synthesizer_agent
 
 def main():
+    console = Console()
     parser = argparse.ArgumentParser(description="Autonomous Competitive Research Swarm")
     parser.add_argument("--topic", type=str, required=True, help="Topic you want to research. Separate multiple topics with commas.")
     parser.add_argument("--persona", type=str, required=True, help="The persona of the agent")
     args = parser.parse_args()
 
     topics = [t.strip() for t in args.topic.split(",")]
-    final_result = run_synthesizer_agent(topics, args.persona)
-    print(final_result)
+    final_result = Markdown(run_synthesizer_agent(topics, args.persona))
+    console.print(final_result)
 
 if __name__ == "__main__":
     main()
